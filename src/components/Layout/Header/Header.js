98ce0cac4c71e655380d0ab2cfe96492ee84c950
monika-pal <monika.pal@moglix.com>
2022-11-24 11:49:03 +0530

    some routing issues fixes

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,19 +1,13 @@
 import { NavLink } from "react-router-dom";
 import React from 'react'; 
- import IMAGES from "../../../assets/images/images.js";
+import IMAGES from "../../../assets/images/images";
+import Menu from "../Menu/Menu";
 
 const Header = () =>{
     return <header className="bg-[#fff] shadow-lg">
         <nav className="flex items-center justify-start px-20 py-5">
-        <img src={ IMAGES.logo_dark } alt="logo" width="180" height="43"/>
-            <ul className="flex items-center ml-8">
-                <li className="px-4 text-base font-bold text-gray-800">
-                <NavLink to='/'>Home</NavLink> 
-                </li>
-                <li className="px-4 text-base font-bold text-gray-800">
-                <NavLink to='/About'>About Us</NavLink>
-                </li>
-            </ul>
+        <NavLink to='/Home'><img src={ IMAGES.logo_dark } alt="logo" width="180" height="43"/></NavLink>
+            <Menu />
         </nav>
     </header>
 };
---
+++ src/components/Layout/Menu/Menu.js
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+const Menu = () =>{
+    return <ul className="flex items-center ml-8">
+    <li className="px-4 text-base font-bold text-gray-800">
+    <NavLink to='/Home'>Home</NavLink> 
+    </li>
+    <li className="px-4 text-base font-bold text-gray-800">
+    <NavLink to='/About'>About Us</NavLink>
+    </li>
+</ul>
+}
+
+export default Menu;
